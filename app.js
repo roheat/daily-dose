5ea4cb58711cfa2f206b5dd05579db24c43f0b41
Rohith Srivathsav <rohithsrivathsav@gmail.com>
2020-04-07 01:16:01 +0530

    nodemon added

--- app.js
+++ app.js
@@ -1,29 +1,26 @@
-var fs = require("fs"),
-  http = require("http"),
-  path = require("path"),
-  methods = require("methods"),
-  express = require("express"),
-  bodyParser = require("body-parser"),
-  session = require("express-session"),
-  cors = require("cors"),
-  passport = require("passport"),
-  errorhandler = require("errorhandler"),
-  mongoose = require("mongoose");
+const fs = require("fs");
+const http = require("http");
+const path = require("path");
+const methods = require("methods");
+const express = require("express");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const cors = require("cors");
+const passport = require("passport");
+const errorhandler = require("errorhandler");
+const mongoose = require("mongoose");
 
-var isProduction = process.env.NODE_ENV === "production";
+const isProduction = process.env.NODE_ENV === "production";
 
-// Create global app object
-var app = express();
+const app = express();
 
 app.use(cors());
 
-// Normal express config defaults
+// log requests
 app.use(require("morgan")("dev"));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-app.use(require("method-override")());
-app.use(express.static(__dirname + "/public"));
 
 app.use(
   session({
@@ -49,7 +46,7 @@ app.use(require("./routes"));
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error("Not Found");
+  const err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
@@ -85,7 +82,7 @@ app.use(function(err, req, res, next) {
   });
 });
 
-// finally, let's start our server...
-var server = app.listen(process.env.PORT || 3000, function() {
+// start the server
+const server = app.listen(process.env.PORT || 3000, function() {
   console.log("Listening on port " + server.address().port);
 });
