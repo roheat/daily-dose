3a920991a50288619fa66320cc34517c27611c85
Rohith Srivathsav <rohithsrivathsav@gmail.com>
2020-04-08 00:44:29 +0530

    user model methods

--- models/User.js
+++ models/User.js
@@ -1,10 +1,15 @@
 const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
+const crypto = require("crypto");
+const jwt = require("jsonwebtoken");
+const secret = require("../config").secret;
 
 const UserSchema = mongoose.Schema(
   {
     username: {
       type: String,
       lowercase: true,
+      unique: true,
       required: [true, "can't be blank"],
       match: [/^[a-zA-Z0-9]+$/, "is invalid"],
       index: true
@@ -12,6 +17,7 @@ const UserSchema = mongoose.Schema(
     email: {
       type: String,
       lowercase: true,
+      unique: true,
       required: [true, "can't be blank"],
       match: [/\S+@\S+\.\S+/, "is invalid"],
       index: true
@@ -24,5 +30,48 @@ const UserSchema = mongoose.Schema(
   // createdAt, updatedAt
   { timestamps: true }
 );
+
+// unique username and email
+UserSchema.plugin(uniqueValidator, { message: "is already taken" });
+
+UserSchema.methods.setPassword = function(password) {
+  this.salt = crpto.randomBytes(16).toString("hex");
+  this.hash = crypto
+    .pbkdf2Sync(password, this.salt, 10000, 512, "sha512")
+    .toString("hex");
+};
+
+UserSchema.methods.validPassword = function(password) {
+  const hash = crypto
+    .pbkdf2Sync(password, this.salt, 10000, 512, "sha512")
+    .toString("hex");
+  return this.hash === hash;
+};
+
+UserSchema.methods.generateJWT = function() {
+  const today = new Date();
+  const exp = new Date(today);
+  exp.setDate(today.getDate() + 30); // expires in 30 days
+
+  return jwt.sign(
+    {
+      id: this._id,
+      username: this.username,
+      exp: parseInt(exp.getTime() / 1000)
+    },
+    secret
+  );
+};
+
+// send authorized user json to FE
+UserSchema.methods.toAuthJSON = function() {
+  return {
+    username: this.username,
+    email: this.email,
+    token: this.generateJWT(),
+    bio: this.bio,
+    image: this.image
+  };
+};
 
 mongoose.model("User", UserSchema);
