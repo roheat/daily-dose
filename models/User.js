f2af58886ed9bbd89c4f17a1abbd91519437b17d
Rohith Srivathsav <rohithsrivathsav@gmail.com>
2020-04-08 00:19:43 +0530

    user model added

---
+++ models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const UserSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      lowercase: true,
+      required: [true, "can't be blank"],
+      match: [/^[a-zA-Z0-9]+$/, "is invalid"],
+      index: true
+    },
+    email: {
+      type: String,
+      lowercase: true,
+      required: [true, "can't be blank"],
+      match: [/\S+@\S+\.\S+/, "is invalid"],
+      index: true
+    },
+    bio: String,
+    image: String,
+    hash: String,
+    salt: String
+  },
+  // createdAt, updatedAt
+  { timestamps: true }
+);
+
+mongoose.model("User", UserSchema);
