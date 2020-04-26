57538caeba9baed11a0f030b9bf84b15fe0ca37d
Rohith Srivathsav <rohithsrivathsav@gmail.com>
2020-04-27 03:08:21 +0530

    ready for prod deploy🚀

--- app.js
+++ app.js
@@ -38,6 +38,11 @@ if (!isProduction) {
 
 if (isProduction) {
   mongoose.connect(process.env.MONGODB_URI);
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  });
 } else {
   mongoose.connect(process.env.DATABASE);
   mongoose.set("debug", true);
