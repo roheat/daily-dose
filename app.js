4fe00ce8a37e36a71d3deaaf3c0b2ebcc62257e7
Rohith Srivathsav <rohithsrivathsav@gmail.com>
2020-04-27 20:51:10 +0530

    prod fix

--- app.js
+++ app.js
@@ -38,11 +38,11 @@ if (!isProduction) {
 
 if (isProduction) {
   mongoose.connect(process.env.MONGODB_URI);
-  app.use(express.static("client/build"));
+  // app.use(express.static("client/build"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  });
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  // });
 } else {
   mongoose.connect(process.env.DATABASE);
   mongoose.set("debug", true);
